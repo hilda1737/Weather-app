acfea698f357c81505a6188c40143156df1fbcaf
hilda1737 <hildamoraahh@gmail.com>
2022-07-05 11:46:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//  const APIkey="be779e9e2439185ccd05ee12691ba3f0"
+//  document.getElementById("btn").addEventListener("click",function(){
+//    const city=document.getElementById("city").value
+//     const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIkey})
+
+// // function abc(){
+// //     let m = "o"
+
+// //     function def(){
+// //         console.log(m)
+// //     }
+// //     def()
+// // }
+// // abc()
